26d23ed1c72f7eed1d2ff7ea96a17040ea3a8e9b
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-01-18 02:30:51 -0600

    incrementing version to reflect change in config settings

--- SpringSecurityLdapGrailsPlugin.groovy
+++ SpringSecurityLdapGrailsPlugin.groovy
@@ -34,7 +34,7 @@ import org.springframework.security.ldap.userdetails.PersonContextMapper
 
 class SpringSecurityLdapGrailsPlugin {
 
-	String version = '1.0.5'
+	String version = '1.1.0'
 	String grailsVersion = '1.2.3 > *'
 	Map dependsOn = [springSecurityCore: '1.1 > *']
 
