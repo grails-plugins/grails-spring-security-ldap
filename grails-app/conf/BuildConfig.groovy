d90d865541db78fe54360fa3bebaa9f86b4a22c3
Burt Beckwith <beckwithb@vmware.com>
2012-05-22 15:49:03 -0400

    update to grails 2.0 locally, updated to latest jar dependencies, updated to release plugin 2.0.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,26 +1,41 @@
-grails.project.class.dir = 'target/classes'
-grails.project.test.class.dir = 'target/test-classes'
-grails.project.test.reports.dir	= 'target/test-reports'
-grails.project.docs.output.dir = 'docs' // for the gh-pages branch
+grails.project.work.dir = 'target'
+grails.project.docs.output.dir = 'docs/manual' // for the gh-pages branch
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
-	inherits('global')
-
+	inherits 'global'
 	log 'warn'
 
-	repositories {        
-		grailsPlugins()
-		grailsHome()
-		ebr() // SpringSource  http://www.springsource.com/repository
+	repositories {
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
 	}
 
 	dependencies {
-		runtime('org.springframework.security:org.springframework.security.ldap:3.0.4.RELEASE') {
-			transitive = false
+		compile('org.springframework.security:spring-security-ldap:3.0.7.RELEASE') {
+			excludes 'spring-security-core', 'spring-ldap-core', 'ldapsdk', 'apacheds-core',
+			         'apacheds-server-jndi', 'slf4j-log4j12', 'log4j', 'junit',
+			         'mockito-core', 'jmock-junit4'
 		}
-		runtime('org.springframework.ldap:org.springframework.ldap:1.3.0.RELEASE') {
-			transitive = false
+		runtime('org.springframework.ldap:spring-ldap-core:1.3.1.RELEASE') {
+			excludes 'commons-logging', 'commons-lang', 'commons-pool', 'ldapbp',
+			         'spring-core', 'spring-beans', 'spring-tx', 'spring-context',
+			         'spring-jdbc', 'spring-orm', 'junit', 'easymock', 'gsbase',
+			         'commons-logging', 'spring-core', 'spring-beans', 'junit', 'log4j'
+		}
+	}
+
+	plugins {
+		compile ':spring-security-core:1.2.7.3'
+
+		compile(":hibernate:$grailsVersion") {
+			export = false
+		}
+
+		build(':release:2.0.2', ':rest-client-builder:1.0.2') {
+			export = false
 		}
 	}
 }
