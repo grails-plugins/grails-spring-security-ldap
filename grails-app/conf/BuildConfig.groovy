27057c2f74ec25c9a271750ccd6b55e5e2400ea8
Burt Beckwith <burt@burtbeckwith.com>
2015-12-01 16:21:09 -0500

    updated to Spring Security 3.2.9.RELEASE

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		String springSecurityVersion = '3.2.7.RELEASE'
+		String springSecurityVersion = '3.2.9.RELEASE'
 
 		compile "org.springframework.security:spring-security-ldap:$springSecurityVersion", {
 			excludes 'apacheds-core', 'apacheds-core-entry', 'apacheds-protocol-ldap', 'apacheds-protocol-shared',
@@ -25,22 +25,22 @@ grails.project.dependency.resolution = {
 
 		compile 'net.sf.ehcache:ehcache:2.9.0'
 
-		runtime 'org.springframework.ldap:spring-ldap-core:2.0.3.RELEASE', {
+		runtime 'org.springframework.ldap:spring-ldap-core:2.0.4.RELEASE', {
 			excludes 'commons-lang', 'gsbase', 'junit', 'mockito-core', 'powermock-api-mockito', 'powermock-api-support',
-			         'powermock-core', 'powermock-module-junit4', 'powermock-module-junit4-common',
-			         'powermock-reflect', 'slf4j-log4j12', 'spring-beans', 'spring-core', 'spring-data-commons',
-			         'spring-test', 'spring-tx'
+			         'powermock-core', 'powermock-module-junit4', 'powermock-module-junit4-common', 'powermock-reflect',
+			         'slf4j-api', 'slf4j-log4j12', 'spring-beans', 'spring-context', 'spring-core', 'spring-data-commons',
+			         'spring-jdbc', 'spring-orm', 'spring-test', 'spring-tx'
 		}
 	}
 
 	plugins {
-		compile ':spring-security-core:2.0-RC5'
+		compile ':spring-security-core:2.0-RC6'
 
 		compile ':hibernate:3.6.10.14', {
 			export = false
 		}
 
-		build ':release:3.0.1', ':rest-client-builder:2.0.3', {
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
 	}
