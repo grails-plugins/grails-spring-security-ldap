d4042de50fcc9e8e11cd6eb88a20fda42d903beb
Sergio del Amo <sergio.delamo@softamo.com>
2020-03-20 16:29:31 +0100

    Update retrieve-group-rules to Grails 4

--- misc-functional-test-app/retrieve-group-rules/grails-app/init/com/test/Application.groovy
+++ misc-functional-test-app/retrieve-group-rules/grails-app/init/com/test/Application.groovy
@@ -3,6 +3,9 @@ package com.test
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
