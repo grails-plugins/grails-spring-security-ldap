0ebbff939fb76c474ec75ca5d812a241db49ebaf
Sergio del Amo <sergio.delamo@softamo.com>
2020-03-24 09:17:48 +0100

    remove pdf output

--- publish-docs.sh
+++ publish-docs.sh
@@ -18,10 +18,6 @@ git clone https://${GH_TOKEN}@github.com/grails-plugins/grails-spring-security-l
 
 cd gh-pages
 
-git rm v4/spring-security-ldap-*.pdf
-mv ../build/docs/spring-security-ldap-*.pdf v4
-git add v4/spring-security-ldap-*.pdf
-
 mv ../build/docs/index.html v4
 git add v4/index.html
 
