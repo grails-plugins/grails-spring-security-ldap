ec1fbafc7fd0022631fb9550c1bba324ec325adc
Sergio del Amo <sergio.delamo@softamo.com>
2020-03-24 07:39:46 +0100

    add logging to travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -4,13 +4,33 @@ set -e
 
 EXIT_STATUS=0
 
+echo "TRAVIS_TAG          : $TRAVIS_TAG"
+echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
+
+
+
+echo "*******************************"
+echo "grails-spring-security-ldap"
+echo "*******************************"
+
 ./gradlew clean check install --stacktrace --console=plain || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "grails-spring-security-ldap:check failed => exit $EXIT_STATUS"
+  exit $EXIT_STATUS
+fi
 
 PROJECTS="retrieve-group-rules retrieve-db-roles custom_user_details_context_mapper"
 
 cd functional-test-app
 
 ./gradlew clean check --stacktrace --console=plain || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "functional-test-app:check failed => exit $EXIT_STATUS"
+  exit $EXIT_STATUS
+fi
 
 cd ..
 
@@ -21,6 +41,11 @@ for project in $PROJECTS; do
     cd $project 
 
     ./gradlew clean check --stacktrace --console=plain || EXIT_STATUS=$?
+
+    if [ $EXIT_STATUS -ne 0 ]; then
+        echo "$project check failed => exit $EXIT_STATUS"
+        exit $EXIT_STATUS
+    fi
 
     cd ..
 
